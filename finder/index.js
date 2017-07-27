9b85cc35a5bcadaeb9bd0f7c79d1f3d85c6b76e6
Seren <SerenTera@users.noreply.github.com>
2017-07-27 23:33:13 +0800

    Update index.js

--- finder/index.js
+++ finder/index.js
@@ -15,10 +15,14 @@ module.exports = function playerfinder(dispatch) {
 	})
 	
 	command.add('clearfind',string => {
-		clearTimeout(timer[namestring.indexOf(string.toLowerCase())])
-		timer.splice(namestring.indexOf(string.toLowerCase()),1)
-		namestring.splice(namestring.indexOf(string.toLowerCase()),1)
-		command.message('Cleared finding '+string)
+		if(namestring.includes(event.name.toLowerCase())) {
+			clearTimeout(timer[namestring.indexOf(string.toLowerCase())])
+			timer.splice(namestring.indexOf(string.toLowerCase()),1)
+			namestring.splice(namestring.indexOf(string.toLowerCase()),1)
+			command.message('Cleared finding '+string)
+		}
+		else
+			command.message('Wrong/Invalid/Missing name input')
 	})
 	
 	dispatch.hook('S_USER_PAPERDOLL_INFO', 3, (event) => {
@@ -32,11 +36,3 @@ module.exports = function playerfinder(dispatch) {
 		}
 	})
 	
-	function finder(nametofind,i) {
-		dispatch.toServer('C_REQUEST_USER_PAPERDOLL_INFO',1, {
-			name:namestring
-		})
-		timer[i] = setTimeout(finder(nametofind,i), Interval_Duration)
-	}
-	
-}
