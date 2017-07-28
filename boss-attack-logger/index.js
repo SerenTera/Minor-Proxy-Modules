28b5a22f7bc55fab6ec47055e6748a8511791424
Seren <SerenTera@users.noreply.github.com>
2017-07-29 00:08:21 +0800

    Update index.js

--- boss-attack-logger/index.js
+++ boss-attack-logger/index.js
@@ -2,7 +2,8 @@ const Command = require('command'),
 		fs = require('fs'),
 		path= require('path')
 		
-let enabled=true
+let enabled=false,
+	showskill=true
 // Notes: file name is date_huntingzoneid_templateid.json
 // Use available data to find out what the boss is from the ids(Use shinra data)
 
@@ -19,15 +20,19 @@ module.exports = function bossattacks(dispatch) {
 	
 
 	dispatch.hook('S_BOSS_GAGE_INFO',2,(event) => {
-		if(enabled)
+		if(enabled) {
 			bossinfo = (parseInt(event.huntingZoneId) + '_' + parseInt(event.templateId)).toString(),
 			bossid = event.id
+		}
 	})
 	
 	dispatch.hook('S_ACTION_STAGE',1,(event) => {
 		if(enabled) {
-			if(bossid.equals(event.source)) 
-				fs.appendFileSync(path.join(__dirname,(Date().slice(4,9)+'_'+bossinfo+'.json')),('['+Date().slice(16,24)+'] Skill:'+event.skill.toString()+' Stage:'+event.stage.toString()+'\r\n'))
+			if(bossid.equals(event.source)) {
+				fs.appendFileSync(path.join(__dirname,(Date().slice(4,10)+'_'+bossinfo+'.json')),('['+Date().slice(16,24)+'] Skill:'+event.skill.toString()+' Stage:'+event.stage.toString()+'\r\n'))
+				if(showskill) command.message(event.skill)
+			}
 		}
 	})
+	
 }
