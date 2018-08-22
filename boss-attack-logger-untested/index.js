c347f7e621b1b21494e5e55fe69452b734f08d36
Seren <cmingxuan94@gmail.com>
2018-08-22 16:50:28 +0800

    Add files via upload

--- boss-attack-logger-untested/index.js
+++ boss-attack-logger-untested/index.js
@@ -1,6 +1,5 @@
-const Command = require('command'),
-		fs = require('fs'),
-		path= require('path')
+const fs = require('fs'),
+	  path= require('path')
 		
 let enabled=false,
 	showskill=false,
@@ -9,23 +8,21 @@ let enabled=false,
 // Use available data to find out what the boss is from the ids(Use shinra data)/use datacenter datas
 //'S_DUNGEON_EVENT_GAGE'
 
-module.exports = function bossattacks(dispatch) {
-	const command = Command(dispatch)
-	
+module.exports = function bossattacks(mod) {
 	let bossid = 0,
 		bossinfo=''
-	
-	command.add('bosslogger', () => {
+////Commands	
+	mod.command.add('bosslogger', () => {
 		enabled = !enabled
-		command.message(enabled ? '(Bosslogger) Enabled' : '(Bosslogger) Disabled')
+		mod.command.message(enabled ? '(Bosslogger) Enabled' : '(Bosslogger) Disabled')
 	})
 	
-	command.add('bossshowskill', () => {
+	mod.command.add('bossshowskill', () => {
 		showskill = !showskill
-		command.message(showskill ? '(Bosslogger) Show skill id Enabled' : '(Bosslogger) Show skill id Disabled')
+		mod.command.message(showskill ? '(Bosslogger) Show skill id Enabled' : '(Bosslogger) Show skill id Disabled')
 	})
-	
-	dispatch.hook('S_BOSS_GAGE_INFO',2,(event) => {
+////Dispatches	
+	mod.hook('S_BOSS_GAGE_INFO',3,(event) => {
 		if(enabled) {
 			bossinfo = (parseInt(event.huntingZoneId) + '_' + parseInt(event.templateId)).toString(),
 			bossid = event.id
@@ -34,38 +31,38 @@ module.exports = function bossattacks(dispatch) {
 		}
 	})
 	
-	dispatch.hook('S_ACTION_STAGE', 1, (event) => {
+	mod.hook('S_ACTION_STAGE',  mod.majorPatchVersion >= 74 ? 7 : 6, (event) => {
 		if(enabled) {
-			if(event.source-bossid==0) {
-				fs.appendFileSync(path.join(__dirname,(Date().slice(4,10)+'_'+bossinfo+'.json')),('['+Date().slice(16,24)+'] Skill:'+event.skill.toString()+' Stage:'+event.stage.toString()+' model:'+event.model+'\r\n'))
-				if(showskill) command.message(event.skill)
+			if(event.gameId-bossid==0) {
+				fs.appendFileSync(path.join(__dirname,(Date().slice(4,10)+'_'+bossinfo+'.json')),('['+Date().slice(16,24)+'] Skill:'+JSON.stringify(event.skill)+' Stage:'+event.stage.toString()+' model:'+event.templateId+' target:'+event.target.toString()+'\r\n'))
+				if(showskill) mod.command.message(JSON.stringify(event.skill));
 			}
 		}
 	})
 	
-	dispatch.hook('S_DUNGEON_EVENT_MESSAGE', 1, event => {
+	mod.hook('S_DUNGEON_EVENT_MESSAGE', 1, event => {
 		if(enabled) {
 			fs.appendFileSync(path.join(__dirname,(Date().slice(4,10)+'_event_'+bossinfo+'.json')),('['+Date().slice(16,24)+'] Message:'+event.message+'\r\n'))
 		}
 	})
 	
-	dispatch.hook('S_ABNORMALITY_BEGIN', 2, event => {
+	mod.hook('S_ABNORMALITY_BEGIN', 2, event => {
 		if(enabled) {
 			if(event.target-bossid==0 && (event.source-bossid==0 || event.source==0)) {
-				fs.appendFileSync(path.join(__dirname,(Date().slice(4,10)+'_'+bossinfo+'.json')),('['+Date().slice(16,24)+'] Abnormality:'+event.id.toString()+' Stack:'+event.stacks.toString()+'\r\n'))
-				if(showskill) command.message(event.id)
+				fs.appendFileSync(path.join(__dirname,(Date().slice(4,10)+'_'+bossinfo+'.json')),('['+Date().slice(16,24)+'] Abnormality:'+event.id.toString()+' Stack:'+event.stacks.toString()+'\r\n'));
+				//if(showskill) mod.command.message(event.id.toString())
 			}
 		}
 	})
 	
-	dispatch.hook('S_DUNGEON_EVENT_GAGE', 1, event => {
+	/*mod.hook('S_DUNGEON_EVENT_GAGE', 1, event => {
 		if(showgageinfo) {
 			fs.appendFileSync(path.join(__dirname,(Date().slice(4,10)+'_gage_'+bossinfo+'.json')),('['+Date().slice(16,24)+'] Type:'+event.type+' Value:'+event.value+' gageType:'+event.gageType+' message:'+event.message+'\r\n'))
 		}
-	})
+	})*/
 
 	
-	dispatch.hook('S_QUEST_BALLOON', 1, event => {
+	mod.hook('S_QUEST_BALLOON', 1, event => {
 		if(enabled) {
 			fs.appendFileSync(path.join(__dirname,(Date().slice(4,10)+'_Quest_'+bossinfo+'.json')),('['+Date().slice(16,24)+'] QuestMessageId:'+event.message.replace(/\D/g,'')+'\r\n'))
 		}
